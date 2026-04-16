a5a3523fb18949507658ae4affa4a7f1935f90f4
shahid-ali6307 <shahidaliplutian@gmail.com>
2026-04-16 11:09:19 +0530

    updated once more

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -5,15 +5,15 @@ export default function renderTaskForm(container){
          <div class="form-section">
             <h2>Add Task</h2>
             <form id="task-form">
-                <input type="text" name="title" required />
-                <textarea name="description"></textarea>
-                <select name="priority">
+                <input type="text" name="title" placeholder="Title" required />
+                <textarea name="description" placeholder="Description"></textarea>
+                <select name="priority" placeholder="Priority">
                     <option value="low">Low</option>
                     <option value="medium">Medium</option>
                     <option value="high">High</option>
                 </select>
-                <input type="date" name="dueDate" />
-                <input type="text" name="tags" placeholder="Comma Seperated" />
+                <input type="date" name="dueDate" placeholder="Duedate"/>
+                <input type="text" name="tags" placeholder="Comma Seperated tags" />
                 <button type="submit">Add Task</button>
             </form>
           </div>
